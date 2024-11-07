c4f9721a92cd3f8d2729c8b7ec20b1d810c3584c
Fayçal Beghalia <faycalbeghalia@outlook.fr>
2024-11-07 14:56:41 +0100

    fixed malloc

--- src/my_malloc.c
+++ src/my_malloc.c
@@ -1,13 +1,13 @@
 #include "my_malloc.h"
 
-#include <stddef.h>
-
 #include "allocator.h"
 
+// Global allocator.
 static blk_allocator *blka;
 
 void *my_malloc(size_t size)
 {
+    // Check if we need to create an allocator.
     if (!blka)
     {
         blka = blk_init_allocator(size);
@@ -16,13 +16,17 @@ void *my_malloc(size_t size)
     // Lock the mutex.
     pthread_mutex_lock(&blka->lock);
 
-    return blk_malloc(blka, size);
+    // Call blk_malloc.
+    void *ptr = blk_malloc(blka, size);
 
     // Unlock the mutex.
     pthread_mutex_unlock(&blka->lock);
+
+    return ptr;
 }
 void my_free(void *ptr)
 {
+    // If no allocator, do nothing.
     if (!blka)
     {
         return;
@@ -31,6 +35,7 @@ void my_free(void *ptr)
     // Lock the mutex.
     pthread_mutex_lock(&blka->lock);
 
+    // Call blk_free.
     blk_free(blka, ptr);
 
     // Unlock the mutex.
@@ -39,6 +44,7 @@ void my_free(void *ptr)
 
 void *my_realloc(void *ptr, size_t size)
 {
+    // Check if we need to create an allocator.
     if (!blka)
     {
         blka = blk_init_allocator(size);
@@ -47,8 +53,10 @@ void *my_realloc(void *ptr, size_t size)
     // Lock the mutex.
     pthread_mutex_lock(&blka->lock);
 
+    // If no ptr, realloc = malloc.
     if (!ptr)
     {
+        // Call blk_malloc.
         ptr = blk_malloc(blka, size);
 
         // Unlock the mutex.
@@ -57,8 +65,10 @@ void *my_realloc(void *ptr, size_t size)
         return ptr;
     }
 
+    // If size = 0, realloc = free.
     if (!size)
     {
+        // Call blk_free.
         blk_free(blka, ptr);
 
         // Unlock the mutex.
@@ -67,6 +77,7 @@ void *my_realloc(void *ptr, size_t size)
         return ptr;
     }
 
+    // Call blk_realloc.
     ptr = blk_realloc(blka, ptr, size);
 
     // Unlock the mutex.
--- src/my_malloc.h
+++ src/my_malloc.h
@@ -4,8 +4,11 @@
 #include <stddef.h>
 
 void *my_malloc(size_t size);
+
 void my_free(void *ptr);
+
 void *my_realloc(void *ptr, size_t size);
+
 void *my_calloc(size_t nmemb, size_t size);
 
 #endif /* ! MY_MALLOC_H */
